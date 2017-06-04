5ebd1f93296b67a26c78b5d6d8d621a687327dd8
Dorota Leniec <dorleniec@gmail.com>
2017-06-04 22:01:42 +0200

    basick theme - init

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+
+function learningWP_resources(){
+    wp_enqueue_style('style',get_stylesheet_uri());
+}
+add_action('wp_enqueue_scripts','learningWP_resources');
+
+//Navigation Menus
+register_nav_menus( array (
+    'primary' => __('Primary Menu'),
+    'footer' => __('Footer Menu'),
+) );
